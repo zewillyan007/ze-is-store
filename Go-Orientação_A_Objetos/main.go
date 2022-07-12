6ac0b7be073a71e9566ed8f3d0f52ff45e5c537e
José Willyan <willyansilvagomes2020@gmail.com>
2022-07-12 13:56:10 -0300

    aula 2

--- Go-Orientação_A_Objetos/main.go
+++ Go-Orientação_A_Objetos/main.go
@@ -9,6 +9,16 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
 	contaDoWillyan := ContaCorrente{titular: "Willyan",
 		numeroAgencia: 0001, numeroConta: 155478, saldo: 135.5}
@@ -17,4 +27,13 @@ func main() {
 
 	fmt.Println(contaDoWillyan)
 	fmt.Println(contaDaBruna)
+
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
+
+	fmt.Println(contaDaSilvia.saldo)
+
+	fmt.Println(contaDaSilvia.Sacar(400))
+	fmt.Println(contaDaSilvia.saldo)
 }
