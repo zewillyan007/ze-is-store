f7ecbcf0428de28c8f8167c960529663e22d4d9b
José Willyan <willyansilvagomes2020@gmail.com>
2022-07-22 14:18:34 -0300

    aprendendo orientação a objeto em go

--- go-orientacao-a-objetos/main/main.go
+++ go-orientacao-a-objetos/main/main.go
@@ -6,6 +6,14 @@ import (
 	"go-orientacao-a-objetos/contas"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaDoWillyan := contas.ContaCorrente{
 		Titular: clientes.Titular{
@@ -14,6 +22,8 @@ func main() {
 			Profissao: "DEV"},
 		NumeroAgencia: 0001,
 		NumeroConta:   155478}
+	contaDoWillyan.Depositar(500)
+	PagarBoleto(&contaDoWillyan, 250)
 
 	fmt.Println(contaDoWillyan.Obtersaldo())
 
@@ -21,6 +31,8 @@ func main() {
 	contaDaSilvia.Titular = clientes.Titular{Nome: "Silvia", CPF: "485.887.502-25", Profissao: "Engenheira"}
 	contaDaSilvia.NumeroAgencia = 0001
 	contaDaSilvia.NumeroConta = 441510205
+	contaDaSilvia.Depositar(1000)
+	PagarBoleto(&contaDaSilvia, 55)
 
 	fmt.Println(contaDaSilvia.Sacar(400))
 	fmt.Println(contaDaSilvia.Obtersaldo())
