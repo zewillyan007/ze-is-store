358649e8c614bee8e36855b0402b4078b0738c7d
José Willyan <willyansilvagomes2020@gmail.com>
2022-07-22 11:00:06 -0300

    aprendendo go

--- go-orientacao-a-objetos/main/main.go
+++ go-orientacao-a-objetos/main/main.go
@@ -13,19 +13,15 @@ func main() {
 			CPF:       "150.574.041-11",
 			Profissao: "DEV"},
 		NumeroAgencia: 0001,
-		NumeroConta:   155478,
-		Saldo:         135.5}
+		NumeroConta:   155478}
 
-	fmt.Println(contaDoWillyan)
+	fmt.Println(contaDoWillyan.Obtersaldo())
 
 	contaDaSilvia := contas.ContaCorrente{}
 	contaDaSilvia.Titular = clientes.Titular{Nome: "Silvia", CPF: "485.887.502-25", Profissao: "Engenheira"}
 	contaDaSilvia.NumeroAgencia = 0001
 	contaDaSilvia.NumeroConta = 441510205
-	contaDaSilvia.Saldo = 500
-
-	fmt.Println(contaDaSilvia.Saldo)
 
 	fmt.Println(contaDaSilvia.Sacar(400))
-	fmt.Println(contaDaSilvia.Saldo)
+	fmt.Println(contaDaSilvia.Obtersaldo())
 }
